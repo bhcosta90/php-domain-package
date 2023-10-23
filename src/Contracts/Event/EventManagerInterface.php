426148f71cd9cfaac77528f4d26bf750ccfe1717
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-23 17:27:08 -0300

    wip

--- src/Contracts/Event/EventManagerInterface.php
+++ src/Contracts/Event/EventManagerInterface.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace BRCas\CA\Contracts\Event;
 
+use Costa\Entity\Contracts\EventInterface;
+
 interface EventManagerInterface
 {
     public function dispatch(EventInterface $event);
