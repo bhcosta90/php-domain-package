b688d22ecd5f411afaca8ae4fc86711edbaf16b5
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-25 19:40:00 -0300

    wip

--- tests/Unit/ValueObject/PasswordTest.php
+++ tests/Unit/ValueObject/PasswordTest.php
@@ -3,31 +3,30 @@
 declare(strict_types=1);
 
 use BRCas\CA\ValueObject\Password;
-
 use Costa\Entity\Contracts\ValueObjectInterface;
 
 use function PHPUnit\Framework\assertInstanceOf;
 use function PHPUnit\Framework\assertIsString;
 use function PHPUnit\Framework\assertTrue;
 
-describe("Password Unit Test", function(){
-    test("Make a password", function(){
+describe("Password Unit Test", function () {
+    test("Make a password", function () {
         $password = Password::make(password: '123456');
         assertInstanceOf(ValueObjectInterface::class, $password);
-        assertIsString((string) $password);
+        assertIsString((string)$password);
     });
 
-    test("Creating a new password", function(){
+    test("Creating a new password", function () {
         $password = new Password(value: '123456');
         assertInstanceOf(ValueObjectInterface::class, $password);
     });
 
-    test("Login with a password", function(){
+    test("Login with a password", function () {
         $password = new Password(value: '$2y$10$Aq661ddSO9FqxCaxTDiO9Oc6BW4aNN8QqhAh8Ts.GtLSbX3Deszdy');
         assertTrue($password->login('123456'));
     });
 
-    test("Exception when do not passed password", function(){
+    test("Exception when do not passed password", function () {
         expect(fn() => Password::make(null))->toThrow('password do not passed');
     });
 });
